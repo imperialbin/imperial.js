338e70b00152a271d16ab93f0412b14784180203
pxseu <57842793+pxseu@users.noreply.github.com>
2021-04-05 03:42:00 +0200

    helper to create links

--- src/lib/utils/links.ts
+++ src/lib/utils/links.ts
@@ -1,8 +1,8 @@
 import type { Imperial } from "../Imperial";
 
 export const createFormatedLink = (client: Imperial, id: string): string => {
-	return `https://${client.Hostname}/p/${id}`;
+	return `https://${client.hostname}/p/${id}`;
 };
 export const createRawLink = (client: Imperial, id: string): string => {
-	return `https://${client.Hostname}/r/${id}`;
+	return `https://${client.hostname}/r/${id}`;
 };
