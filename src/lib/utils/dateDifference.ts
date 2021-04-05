98d1722139990f334e2c130b33b7049f8d4eac69
pxseu <57842793+pxseu@users.noreply.github.com>
2021-04-05 03:42:38 +0200

    luttle function to get difference in days between two dates

---
+++ src/lib/utils/dateDifference.ts
@@ -0,0 +1,9 @@
+/**
+ *  Little helper to get done with dates easier
+ * @param firstDate Current date or the creation date
+ * @param secondDate The deletion date
+ */
+
+export const getDateDifference = (firstDate: Date, secondDate: Date): number => {
+	return Math.round((secondDate.getTime() - firstDate.getTime()) / (1000 * 60 * 60 * 24));
+};
