37025758584e52fb3c53ff111f1fc71a0072aad2
pxseu <57842793+pxseu@users.noreply.github.com>
2021-07-28 08:14:15 +0200

    fix: remove constructor of FailedToFetch error class

--- src/lib/errors/HTTPErrors/FailedToFetch.ts
+++ src/lib/errors/HTTPErrors/FailedToFetch.ts
@@ -1,9 +1,3 @@
 import { ImperialError } from "../ImperialError";
 
-export class FailedToFetch extends ImperialError {
-	constructor({ message, ...args }: ConstructorParameters<typeof ImperialError>[0] = {}) {
-		super(args);
-
-		this.message = "Requst was aborted.";
-	}
-}
+export class FailedToFetch extends ImperialError {}
