8172a0c0986ccebf222e69f752ae08cac10471a8
Cody Miller <hello@looskie.com>
2023-06-03 11:16:04 -0400

    feat: export permissions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,5 +4,7 @@ export * from "./client/Imperial";
 export * from "./classes";
 // Export all errors for the user
 export * from "./errors";
+// Export permissions
+export * from "./utils/Permissions";
 // export Imperial as default
 export { Imperial as default } from "./client/Imperial";
--- src/lib/utils/Permissions.ts
+++ src/lib/utils/Permissions.ts
@@ -2,4 +2,4 @@ import { Permer } from "permer";
 
 const permer = new Permer(["member", "member-plus", "beta-tester", "admin"]);
 
-export { permer };
+export { permer as ImperialPermissions };
